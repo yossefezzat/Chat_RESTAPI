205c86921125959c2b28410fadcb925981f948ea
youssef ezzat <youssef.ezzat@intellaworld.com>
2022-06-11 21:42:07 +0200

    Edit configurations for docker compose

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,1 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_URL'] || "localhost:9200"
+Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_URL'] 
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-    config.redis = { url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
+    config.redis = { url: ENV['REDIS_URL_SIDEKIQ'] }
 end
   
 Sidekiq.configure_client do |config|
-    config.redis = { url: ENV.fetch('REDIS_URL_SIDEKIQ', 'redis://localhost:6379/1') }
+    config.redis = { url: ENV['REDIS_URL_SIDEKIQ'] }
 end

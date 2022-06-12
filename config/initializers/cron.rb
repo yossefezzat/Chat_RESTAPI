4e3b83c033ba9cf7545faed5fe289a8f8515d4a8
youssef ezzat <youssef.ezzat@intellaworld.com>
2022-06-12 11:34:39 +0200

    Edit in cron job to be run every 1 hr

--- config/initializers/cron.rb
+++ config/initializers/cron.rb
@@ -1,4 +1,4 @@
 Rails.application.reloader.to_prepare do
-    Sidekiq::Cron::Job.create(name: "Update apps' chats_count - every 1hr", cron: '*/50 * * * *' , class: 'UpdateChatsCountWorker')
-    Sidekiq::Cron::Job.create(name: "Update chats' messages_count - every 1hr", cron: '*/52 * * * *' , class: 'UpdateMessagesCountWorker')
+    Sidekiq::Cron::Job.create(name: "Update apps' chats_count - every 1hr", cron: '0 * * * *' , class: 'UpdateChatsCountWorker')
+    Sidekiq::Cron::Job.create(name: "Update chats' messages_count - every 1hr", cron: '0 * * * *' , class: 'UpdateMessagesCountWorker')
 end

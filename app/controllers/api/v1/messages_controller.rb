9bf284ec2a6c94b32653b97109e65406118bb77d
youssef ezzat <ye.fci2020@gmail.com>
2022-06-13 22:57:02 +0200

    Enhance in messages retrievals queries

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -4,17 +4,24 @@ class Api::V1::MessagesController < ApplicationController
     def index  # TODO: enhance it later 
         app_key = params[:app_key]
         chat_number = params[:chat_number]
-        app = Application.find_by_app_key(app_key)
-        unless app.nil?
-            chat = Chat.find_by({application_id: app.id, number: chat_number})
-            unless chat.nil?
-                render json: {STATUS: "SUCCESS", messages: chat.messages }, status: :ok
-            else
-                render json: {STATUS: "ERROR", error: "Chat not found"}, status: :not_found
-            end
+        check_chat = REDIS.hexists(app_key, chat_number)
+        if check_chat
+            messages = Message.where('app_key' => app_key, 'chat_id'=> chat_number)
+            render json: messages
         else
-            render json: {STATUS: "ERROR", error: "App not found"}, status: :not_found
+            render json: {status: 'ERROR', error: "Not found"}, status: :not_found
         end
+        # app = Application.find_by_app_key(app_key)
+        # unless app.nil?
+        #     chat = Chat.find_by({application_id: app.id, number: chat_number})
+        #     unless chat.nil?
+        #         render json: {STATUS: "SUCCESS", messages: chat.messages }, status: :ok
+        #     else
+        #         render json: {STATUS: "ERROR", error: "Chat not found"}, status: :not_found
+        #     end
+        # else
+        #     render json: {STATUS: "ERROR", error: "App not found"}, status: :not_found
+        # end
     end
     
 
